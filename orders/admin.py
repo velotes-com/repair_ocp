36ec9facd63887af143fe313f32363cc3ad98fce
velotes-com <velotes@ya.ru>
2025-09-30 15:23:39 +0300

    update admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -4,8 +4,15 @@ from .models import Order
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["number", "status", "client_name", "client_phone", "device_model", "problem_description", "final_price"]
-    list_display_links = ["number"]
+    list_display = ["formatted_number", "status", "client_name", "client_phone", "device_model", "problem_description",
+                    "final_price"]
+    list_display_links = ["formatted_number"]
 
+    def formatted_number(self, obj):
+        return f"Заказ № {obj.number:04d}"
+
+    formatted_number.short_description = "Номер заказа"  # Заголовок колонки
+    formatted_number.admin_order_field = 'number'  # Для возможности сортировки
+
+# admin.site.register(Order)
 admin.site.register(Order, PostAdmin)
-# admin.site.register()
