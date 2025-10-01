a1d59a455e308a0330aebcb13a3f20777df2342e
velotes-com <velotes@ya.ru>
2025-10-01 21:57:39 +0300

    update many files

--- orders/admin.py
+++ orders/admin.py
@@ -4,15 +4,17 @@ from .models import Order
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["formatted_number", "status", "client_name", "client_phone", "device_model", "problem_description",
-                    "final_price"]
-    list_display_links = ["formatted_number"]
+    list_display = ("str_number_order", "status", "priority", "client_name", "client_phone", "device_model", "problem_description",
+                    "final_price")
+    list_display_links = ("str_number_order", )
+    list_editable = ("priority", )
+    readonly_fields = ("watch_count", )
 
-    def formatted_number(self, obj):
-        return f"Заказ № {obj.number:04d}"
+    def str_number_order(self, obj):
+        return f"Заказ № {obj.number_order:04d}"
 
-    formatted_number.short_description = "Номер заказа"  # Заголовок колонки
-    formatted_number.admin_order_field = 'number'  # Для возможности сортировки
+    str_number_order.short_description = "Номер заказа"  # Заголовок колонки
+    str_number_order.admin_order_field = 'number_order'  # Для возможности сортировки
 
 # admin.site.register(Order)
 admin.site.register(Order, PostAdmin)
--- orders/models.py
+++ orders/models.py
@@ -12,8 +12,9 @@ class Order(models.Model):
         CLOSED = 'closed', 'Закрыт'
 
     # number = models.IntegerField(unique=True, verbose_name="Номер заказа")
-    number = models.AutoField(primary_key=True, verbose_name='Номер заказа')
+    number_order = models.AutoField(primary_key=True, verbose_name='Номер заказа')
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.NEW, verbose_name='Статус заказа')
+    priority = models.BooleanField(default=False, verbose_name="Приоритет")
     client_name = models.CharField(max_length=55, verbose_name=_('ФИО'), default="Хулиана Ульяна Яковлева")
     client_phone = models.CharField(max_length=15, verbose_name='Номер телефона')
     client_address = models.TextField(verbose_name='Адрес')
@@ -25,10 +26,11 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Изменён')
     # deleted = models.BooleanField(default=False, verbose_name="Удален")
+    watch_count = models.IntegerField(default=0, verbose_name="Количество просмотров заявки")
 
     def __str__(self):
         # return f"Заказ # {self.number} - {self.client_name}"
-        return f"Заказ № {self.number}"
+        return f"Заказ № {self.number_order}"
 
     class Meta:
         verbose_name = "Заказ"
